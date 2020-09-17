d33c814931153b2b10ae6cc13cc799dfdca5f996
38057 <38057@edu.raseko.fi>
2020-09-17 15:51:11 +0300

    Laskettiin rasvaprosentit

--- Sali3/Program.cs
+++ Sali3/Program.cs
@@ -33,7 +33,12 @@ namespace Sali3
 
             Painoindeksi painoindeksi = new Painoindeksi("10.9.2020", "1,93", "106,5");
 
+            // Testataan rasvaprosentin laskemista
+
+            Rasvaprosentti rasvaprosentti = new Rasvaprosentti();
+
             // Katsotaan olioden ominaisuuksia
+
             Console.WriteLine("Testi oletusmuodostimelle");
             henkilo.naytaHenkilo();
             Console.WriteLine("Testi muodostimelle kaikilla parametreillä");
@@ -45,6 +50,25 @@ namespace Sali3
             Console.WriteLine("Testi painoindeksin muodostimelle");
             painoindeksi.naytaBMI();
             Console.WriteLine("Painoindeksi on " + painoindeksi.BMI());
+
+            // Testataan rasvaprosenttilaskenta
+
+            rasvaprosentti.Ika = "27";
+            rasvaprosentti.Sukupuoli = "Mies";
+            Console.WriteLine("Ikäsi on " + rasvaprosentti.Ika);
+            Console.WriteLine("Sukupuolesi on " + rasvaprosentti.Sukupuoli);
+            Console.WriteLine("Rasvaprosenttisi on " + rasvaprosentti.RasvaAikuinen(1.93f, 106f));
+
+            // Testataan naisen rasvaprosenttilaskenta
+
+            Rasvaprosentti nainen = new Rasvaprosentti("Nainen", "25");
+            
+            Console.WriteLine("Naisen rasvaprosentti on " + nainen.RasvaAikuinen(1.71f, 59f));
+
+            // Testataan staattista rasvaprosentti metodia
+
+            Console.WriteLine("Rasvaprosentti on " + Rasvaprosentti.laskeRasva(106.7f, 1.93f, 27, "Mies"));
+
         }
     }
 }
---
+++ Sali3/Rasvaprosentti.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Sali3
+{
+    class Rasvaprosentti
+    {
+        public string Sukupuoli { get; set; }
+        public string Ika { get; set; }
+
+        // Oletusmuodostin
+
+        public Rasvaprosentti()
+        {
+            this.Sukupuoli = "Mies";
+            this.Ika = "0";
+        }
+
+        // Metodi kaikilla parametreillä
+
+        public Rasvaprosentti(string sukupuoli, string ika)
+        {
+            this.Sukupuoli = sukupuoli;
+            this.Ika = ika;
+        }
+
+        public float RasvaAikuinen(float pituus, float paino)
+        {
+            float BMI = paino / (pituus * pituus);
+            float sukupkerroin = 0;
+            if (this.Sukupuoli == "mies")
+            {
+                sukupkerroin = 1;
+            }
+        
+            float rasva = 1.2f * BMI + 0.23f * float.Parse(this.Ika) - 10.8f * sukupkerroin - 5.4f;
+            return rasva;
+           
+        }
+
+        // Staattinen lasku metodi, jolla rasvaprosentin voi laskea ilman oliota
+
+        static public float laskeRasva(float paino, float pituus, float ika, string sukupuoli)
+        {
+            float BMI = paino / (pituus * pituus);
+            float sukukerroin = 0;
+            sukupuoli = sukupuoli.ToLower();
+            if (sukupuoli == "mies")
+            {
+                sukukerroin = 1;
+            }
+            float rasva = 1.2f * BMI + 0.23f * ika - 10.8f * sukukerroin - 5.4f;
+            return rasva;
+        }
+    }
+}
